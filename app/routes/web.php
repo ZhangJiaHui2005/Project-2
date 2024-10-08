10e77e06853fa5ba4a4f926bf2e64c17554cb7d7
DESKTOP-1JPHE9U\huymindhack <huyarkadata@gmail.com>
2024-10-08 20:46:43 +0700

    update customer apperance

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(['prefix' => 'customer'], function () {
+    Route::get('/', [CustomerController::class,'index'])->name('customers.index');
 });
 
 require __DIR__.'/auth.php';
