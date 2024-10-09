1a1dbb5c29c384cefa34932db3d8bfc0846e9456
huymindhack <huyarkadata@gmail.com>
2024-10-09 13:32:29 +0700

    update

---
+++ app/resources/views/includes/head.blade.php
@@ -0,0 +1,27 @@
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/fevicon.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Edgecut</title>
+
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
+
+  <!-- font awesome style -->
+  <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('css/style.css') }}?v=1.0" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="{{asset('css/responsive.css')}}" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
